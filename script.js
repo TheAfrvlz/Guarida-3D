10a43f832ed14d33aa7282c415d454b81f9b9667
Jesus ( AfranVelz ) Velez Soto <44423689+Avelz19@users.noreply.github.com>
2021-07-04 13:22:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,307 @@
+var modal = document.getElementsByClassName("modal");
+var btn = document.getElementsByClassName("myBTN");
+var span = document.getElementsByClassName("closeBTN");
+
+btn[0].onclick = function () {
+  modal[0].style.display = "block";
+};
+
+span[0].onclick = function () {
+  modal[0].style.display = "none";
+};
+
+btn[1].onclick = function () {
+  modal[1].style.display = "block";
+};
+
+span[1].onclick = function () {
+  modal[1].style.display = "none";
+};
+
+btn[2].onclick = function () {
+  modal[2].style.display = "block";
+};
+
+span[2].onclick = function () {
+  modal[2].style.display = "none";
+};
+btn[3].onclick = function () {
+  modal[3].style.display = "block";
+};
+
+span[3].onclick = function () {
+  modal[3].style.display = "none";
+};
+
+btn[4].onclick = function () {
+  modal[4].style.display = "block";
+};
+
+span[4].onclick = function () {
+  modal[4].style.display = "none";
+};
+
+btn[5].onclick = function () {
+  modal[5].style.display = "block";
+};
+
+span[5].onclick = function () {
+  modal[5].style.display = "none";
+};
+btn[6].onclick = function () {
+  modal[6].style.display = "block";
+};
+
+span[6].onclick = function () {
+  modal[6].style.display = "none";
+};
+
+btn[7].onclick = function () {
+  modal[7].style.display = "block";
+};
+
+span[7].onclick = function () {
+  modal[7].style.display = "none";
+};
+
+btn[8].onclick = function () {
+  modal[8].style.display = "block";
+};
+
+span[8].onclick = function () {
+  modal[8].style.display = "none";
+};
+btn[9].onclick = function () {
+  modal[9].style.display = "block";
+};
+
+span[9].onclick = function () {
+  modal[9].style.display = "none";
+};
+
+btn[10].onclick = function () {
+  modal[10].style.display = "block";
+};
+
+span[10].onclick = function () {
+  modal[10].style.display = "none";
+};
+
+btn[11].onclick = function () {
+  modal[11].style.display = "block";
+};
+
+span[11].onclick = function () {
+  modal[11].style.display = "none";
+};
+btn[12].onclick = function () {
+  modal[12].style.display = "block";
+};
+
+span[12].onclick = function () {
+  modal[12].style.display = "none";
+};
+btn[13].onclick = function () {
+  modal[13].style.display = "block";
+};
+
+span[13].onclick = function () {
+  modal[13].style.display = "none";
+};
+
+btn[14].onclick = function () {
+  modal[14].style.display = "block";
+};
+
+span[14].onclick = function () {
+  modal[14].style.display = "none";
+};
+
+btn[15].onclick = function () {
+  modal[15].style.display = "block";
+};
+
+span[15].onclick = function () {
+  modal[15].style.display = "none";
+};
+btn[16].onclick = function () {
+  modal[16].style.display = "block";
+};
+
+span[16].onclick = function () {
+  modal[16].style.display = "none";
+};
+
+
+btn[17].onclick = function () {
+  modal[17].style.display = "block";
+};
+
+span[17].onclick = function () {
+  modal[17].style.display = "none";
+};
+
+btn[18].onclick = function () {
+  modal[18].style.display = "block";
+};
+
+span[18].onclick = function () {
+  modal[18].style.display = "none";
+};
+btn[19].onclick = function () {
+  modal[19].style.display = "block";
+};
+
+span[19].onclick = function () {
+  modal[19].style.display = "none";
+};
+btn[20].onclick = function () {
+  modal[20].style.display = "block";
+};
+
+span[20].onclick = function () {
+  modal[20].style.display = "none";
+};
+
+btn[21].onclick = function () {
+  modal[21].style.display = "block";
+};
+
+span[21].onclick = function () {
+  modal[21].style.display = "none";
+};
+btn[22].onclick = function () {
+  modal[22].style.display = "block";
+};
+
+span[22].onclick = function () {
+  modal[22].style.display = "none";
+};
+btn[23].onclick = function () {
+  modal[23].style.display = "block";
+};
+
+span[23].onclick = function () {
+  modal[23].style.display = "none";
+};
+
+btn[24].onclick = function () {
+  modal[24].style.display = "block";
+};
+
+span[24].onclick = function () {
+  modal[24].style.display = "none";
+};
+
+btn[25].onclick = function () {
+  modal[25].style.display = "block";
+};
+
+span[25].onclick = function () {
+  modal[25].style.display = "none";
+};
+
+btn[26].onclick = function () {
+  modal[26].style.display = "block";
+};
+
+span[26].onclick = function () {
+  modal[26].style.display = "none";
+};
+
+btn[27].onclick = function () {
+  modal[27].style.display = "block";
+};
+
+span[27].onclick = function () {
+  modal[27].style.display = "none";
+};
+
+/*window.onclick = function (event) {
+  if (event.target == modal[0]) {
+    modal[0].style.display = "none";
+  }
+  if (event.target == modal[1]) {
+    modal[1].style.display = "none";
+  }
+  if (event.target == modal[2]) {
+    modal[2].style.display = "none";
+  }
+  if (event.target == modal[3]) {
+    modal[3].style.display = "none";
+  }
+  if (event.target == modal[4]) {
+    modal[4].style.display = "none";
+  }
+  if (event.target == modal[5]) {
+    modal[5].style.display = "none";
+  }
+  if (event.target == modal[6]) {
+    modal[6].style.display = "none";
+  }
+  if (event.target == modal[7]) {
+    modal[7].style.display = "none";
+  }
+  if (event.target == modal[8]) {
+    modal[8].style.display = "none";
+  }
+  if (event.target == modal[9]) {
+    modal[9].style.display = "none";
+  }
+  if (event.target == modal[10]) {
+    modal[10].style.display = "none";
+  }
+  if (event.target == modal[11]) {
+    modal[11].style.display = "none";
+  }
+  if (event.target == modal[12]) {
+    modal[12].style.display = "none";
+  }
+  if (event.target == modal[13]) {
+    modal[13].style.display = "none";
+  }
+  if (event.target == modal[14]) {
+    modal[14].style.display = "none";
+  }
+  if (event.target == modal[15]) {
+    modal[15].style.display = "none";
+  }
+  if (event.target == modal[16]) {
+    modal[16].style.display = "none";
+  }
+  if (event.target == modal[17]) {
+    modal[17].style.display = "none";
+  }
+  if (event.target == modal[18]) {
+    modal[18].style.display = "none";
+  }
+  if (event.target == modal[19]) {
+    modal[19].style.display = "none";
+  }
+  if (event.target == modal[20]) {
+    modal[20].style.display = "none";
+  }
+  if (event.target == modal[21]) {
+    modal[21].style.display = "none";
+  }
+  if (event.target == modal[22]) {
+    modal[22].style.display = "none";
+  }
+  if (event.target == modal[23]) {
+    modal[23].style.display = "none";
+  }
+  if (event.target == modal[24]) {
+    modal[24].style.display = "none";
+  }
+  if (event.target == modal[25]) {
+    modal[25].style.display = "none";
+  }
+  if (event.target == modal[26]) {
+    modal[26].style.display = "none";
+  }
+
+  if (event.target == modal[27]) {
+    modal[27].style.display = "none";
+  }
+
+};*/
